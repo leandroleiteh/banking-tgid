c197694353de4cb0d310a90f04f01dfab8beba76
leandroleiteh <leandroluz201616@gmail.com>
2024-02-04 03:02:37 -0300

    fix: add configuration feign and Async

--- src/main/java/com/lebanking/api/LebankingApplication.java
+++ src/main/java/com/lebanking/api/LebankingApplication.java
@@ -2,8 +2,12 @@ package com.lebanking.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableAsync(proxyTargetClass = true)
 public class LebankingApplication {
 
     public static void main(String[] args) {
